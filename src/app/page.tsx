fda9b817c70401da866333ad6b8335ec90f634b4
Temidayo Omoyajowo <temidayoomoyajowo@seamlesshr.com>
2025-12-11 14:12:53 +0100

    feat: setup DB connection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import { Button } from "@/components/ui/button";
-const Page = () => {
+import prisma from "@/lib/database";
+const Page = async () => {
+  const users = await prisma.user.findMany();
   return <div className="min-h-screen min-w-screen flex items-center justify-center">
+    {JSON.stringify(users)}
     <Button variant={"outline"}> Click me</Button>
   </div>;
 };
