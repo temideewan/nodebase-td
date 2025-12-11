aa9e56d69f24552ef7c0d131d1a27deef2d635a5
Temidayo Omoyajowo <temidayoomoyajowo@seamlesshr.com>
2025-12-11 17:02:17 +0100

    feat: database cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import { Button } from "@/components/ui/button";
 import prisma from "@/lib/database";
 const Page = async () => {
   const users = await prisma.user.findMany();
   return <div className="min-h-screen min-w-screen flex items-center justify-center">
     {JSON.stringify(users)}
-    <Button variant={"outline"}> Click me</Button>
   </div>;
 };
 
