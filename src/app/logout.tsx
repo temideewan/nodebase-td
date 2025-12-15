0d3c4b8a0ddb962ea89e72c3e42c164689c5e874
Temidayo Omoyajowo <temidayoomoyajowo@seamlesshr.com>
2025-12-15 20:25:06 +0100

    fix: add router redirects on successful logout

--- src/app/logout.tsx
+++ src/app/logout.tsx
@@ -1,7 +1,23 @@
 'use client';
 import { authClient } from '@/lib/auth-client';
 import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation';
 
 export const LogoutButton = () => {
-  return <Button onClick={() => authClient.signOut()}>Logout</Button>;
+  const router = useRouter();
+  return (
+    <Button
+      onClick={() =>
+        authClient.signOut({
+          fetchOptions: {
+            onSuccess: () => {
+              router.push('/login');
+            },
+          },
+        })
+      }
+    >
+      Logout
+    </Button>
+  );
 };
