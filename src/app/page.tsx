6a80c6e3a3997a46f025b9cbe908a65b60795aa4
Temidayo Omoyajowo <temidayoomoyajowo@seamlesshr.com>
2025-12-18 21:45:08 +0100

    feat: first steps to setting up inngest for background workflows

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,28 @@
-import { requireAuth } from "@/lib/auth-utils";
-import { caller } from "@/trpc/server";
-import { LogoutButton } from "./logout";
+'use client';
+import { caller } from '@/trpc/server';
+import { LogoutButton } from './logout';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { useTRPC } from '@/trpc/client';
+import { Button } from '@/components/ui/button';
 
-const Page = async () => {
-  await requireAuth()
-  const data = await caller.getUsers()
+const Page = () => {
+  const queryClient = useQueryClient();
+  const trpc = useTRPC();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
+  const create = useMutation(
+    trpc.createWorkflow.mutationOptions({
+      onSuccess: () => {
+        queryClient.invalidateQueries(trpc.getWorkflows.queryOptions());
+      },
+    })
+  );
   return (
     <div className='min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6'>
       Protected server component
-      <div>
-      {JSON.stringify(data, null, 2)}
-      </div>
+      <div>{JSON.stringify(data, null, 2)}</div>
+      <Button onClick={() => create.mutate()} disabled={create.isPending}>
+        Create workflow
+      </Button>
       <LogoutButton></LogoutButton>
     </div>
   );
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,10 +1,20 @@
+import { inngest } from '@/inngest/client';
 import { protectedProcedure, createTRPCRouter } from '../init';
 import prisma from '@/lib/database';
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure.query(({ ctx }) => {
-    return prisma.user.findMany({
-      where: {
-        id: ctx.auth.user.id,
+  getWorkflows: protectedProcedure.query(({ ctx }) => {
+    return prisma.workflow.findMany({});
+  }),
+  createWorkflow: protectedProcedure.mutation(async () => {
+    await inngest.send({
+      name: "test/hello.world",
+      data: {
+        email: "temideewan@mail.com"
+      }
+    })
+    return prisma.workflow.create({
+      data: {
+        name: 'test-workflow',
       },
     });
   }),
